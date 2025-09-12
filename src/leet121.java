41804782c879b9cfa0c6ead7bf37022336ce9496
ramingle309 <ramingle309@gmail.com>
2025-09-13 02:32:53 +0530

    leet121

--- src/leet121.java
+++ src/leet121.java
@@ -1,8 +1,21 @@
 public class leet121 {
     public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println(maxProfit(prices));
 
     }
     public static int maxProfit(int[] prices) {
+        int low=prices[0];
+        int max_profit=0;
+        for (int i = 1; i <prices.length ; i++) {
+            if (prices[i]<low){
+                low=prices[i];
+            }
+            if (max_profit<prices[i]-low){
+                max_profit=prices[i]-low;
+            }
+        }
+        return max_profit;
 
     }
 }
